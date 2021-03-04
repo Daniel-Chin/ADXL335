d4766e3a0ef4d4f398f22b49922e12d6dabc434c
Daniel Chin <daniel_chin@yahoo.com>
2021-03-04 13:45:38 +0800

    calibration script

--- Calibration/Calibration.ino
+++ Calibration/Calibration.ino
@@ -1,179 +1,172 @@
 #include "ADXL335.h"
 
-class CircumcentreSolver {
-	// Modified from https://stackoverflow.com/a/13601950/8622053
-	// Original author: June Rhodes, 
-	/// Given four points in 3D space, solves for a sphere such that all four points
-	/// lie on the sphere's surface.
-	/// Translated from Javascript on http://www.convertalot.com/sphere_solver.html, originally
-	float ZERO = 0;
-	double m_X0, m_Y0, m_Z0;
-	double m_Radius;
-	double P[4][3] = {
-		{ ZERO, ZERO, ZERO },
-		{ ZERO, ZERO, ZERO },
-		{ ZERO, ZERO, ZERO },
-		{ ZERO, ZERO, ZERO }
+// Modified from https://stackoverflow.com/a/13601950/8622053
+// Original author: June Rhodes, 
+/// Given four points in 3D space, solves for a sphere such that all four points
+/// lie on the sphere's surface.
+/// Translated from Javascript on http://www.convertalot.com/sphere_solver.html, originally
+float ZERO = 0;
+double m_X0, m_Y0, m_Z0;
+double m_Radius;
+double P[4][3] = {
+	{ ZERO, ZERO, ZERO },
+	{ ZERO, ZERO, ZERO },
+	{ ZERO, ZERO, ZERO },
+	{ ZERO, ZERO, ZERO }
+};
+
+/// <summary>
+/// Whether the result was a valid sphere.
+/// </summary>
+bool isValid() {
+	return m_Radius != 0;
+}
+
+/// <summary>
+/// Computes the centre of a sphere such that all four specified points in
+/// 3D space lie on the sphere's surface.
+/// </summary>
+void CircumcentreSolver(int a[], int b[], int c[], int d[])
+{
+	P[0][0] = (double) a[0];
+	P[0][1] = (double) a[1];
+	P[0][2] = (double) a[2];
+	P[1][0] = (double) b[0];
+	P[1][1] = (double) b[1];
+	P[1][2] = (double) b[2];
+	P[2][0] = (double) c[0];
+	P[2][1] = (double) c[1];
+	P[2][2] = (double) c[2];
+	P[3][0] = (double) d[0];
+	P[3][1] = (double) d[1];
+	P[3][2] = (double) d[2];
+
+	// Compute result sphere.
+	Sphere();
+}
+
+void Sphere() {
+	double r, m11, m12, m13, m14, m15;
+	double a[4][4] = {
+		{ ZERO, ZERO, ZERO, ZERO },
+		{ ZERO, ZERO, ZERO, ZERO },
+		{ ZERO, ZERO, ZERO, ZERO },
+		{ ZERO, ZERO, ZERO, ZERO }
 	};
 
-	/// <summary>
-	/// Whether the result was a valid sphere.
-	/// </summary>
-	bool isValid() {
-		return this->m_Radius != 0;
-	}
-
-	/// <summary>
-	/// Computes the centre of a sphere such that all four specified points in
-	/// 3D space lie on the sphere's surface.
-	/// </summary>
-	/// <param name="a">The first point (array of 3 doubles for X, Y, Z).</param>
-	/// <param name="b">The second point (array of 3 doubles for X, Y, Z).</param>
-	/// <param name="c">The third point (array of 3 doubles for X, Y, Z).</param>
-	/// <param name="d">The fourth point (array of 3 doubles for X, Y, Z).</param>
-	CircumcentreSolver(double a[], double b[], double c[], double d[])
+	// Find minor 1, 1.
+	for (int i = 0; i < 4; i++)
 	{
-		P[0][0] = a[0];
-		P[0][1] = a[1];
-		P[0][2] = a[2];
-		P[1][0] = b[0];
-		P[1][1] = b[1];
-		P[1][2] = b[2];
-		P[2][0] = c[0];
-		P[2][1] = c[1];
-		P[2][2] = c[2];
-		P[3][0] = d[0];
-		P[3][1] = d[1];
-		P[3][2] = d[2];
-
-		// Compute result sphere.
-		this->Sphere();
+		a[i][0] = P[i][0];
+		a[i][1] = P[i][1];
+		a[i][2] = P[i][2];
+		a[i][3] = 1;
 	}
+	m11 = Determinant(a, 4);
 
-	void Sphere() {
-		double r, m11, m12, m13, m14, m15;
-		double a[4][4] = {
-			{ ZERO, ZERO, ZERO, ZERO },
-			{ ZERO, ZERO, ZERO, ZERO },
-			{ ZERO, ZERO, ZERO, ZERO },
-			{ ZERO, ZERO, ZERO, ZERO }
-		};
-
-		// Find minor 1, 1.
-		for (int i = 0; i < 4; i++)
-		{
-			a[i][0] = P[i][0];
-			a[i][1] = P[i][1];
-			a[i][2] = P[i][2];
-			a[i][3] = 1;
-		}
-		m11 = this->Determinant(a, 4);
-
-		// Find minor 1, 2.
-		for (int i = 0; i < 4; i++)
-		{
-			a[i][0] = P[i][0] * P[i][0] + P[i][1] * P[i][1] + P[i][2] * P[i][2];
-			a[i][1] = P[i][1];
-			a[i][2] = P[i][2];
-			a[i][3] = 1;
-		}
-		m12 = this->Determinant(a, 4);
-
-		// Find minor 1, 3.
-		for (int i = 0; i < 4; i++)
-		{
-			a[i][0] = P[i][0] * P[i][0] + P[i][1] * P[i][1] + P[i][2] * P[i][2];
-			a[i][1] = P[i][0];
-			a[i][2] = P[i][2];
-			a[i][3] = 1;
-		}
-		m13 = this->Determinant(a, 4);
-
-		// Find minor 1, 4.
-		for (int i = 0; i < 4; i++)
-		{
-			a[i][0] = P[i][0] * P[i][0] + P[i][1] * P[i][1] + P[i][2] * P[i][2];
-			a[i][1] = P[i][0];
-			a[i][2] = P[i][1];
-			a[i][3] = 1;
-		}
-		m14 = this->Determinant(a, 4);
-
-		// Find minor 1, 5.
-		for (int i = 0; i < 4; i++)
-		{
-			a[i][0] = P[i][0] * P[i][0] + P[i][1] * P[i][1] + P[i][2] * P[i][2];
-			a[i][1] = P[i][0];
-			a[i][2] = P[i][1];
-			a[i][3] = P[i][2];
-		}
-		m15 = this->Determinant(a, 4);
-
-		// Calculate result.
-		if (m11 == 0) {
-			this->m_X0 = 0;
-			this->m_Y0 = 0;
-			this->m_Z0 = 0;
-			this->m_Radius = 0;
-		} else {
-			this->m_X0 = 0.5 * m12 / m11;
-			this->m_Y0 = -0.5 * m13 / m11;
-			this->m_Z0 = 0.5 * m14 / m11;
-			this->m_Radius = sqrt(this->m_X0 * this->m_X0 + this->m_Y0 * this->m_Y0 + this->m_Z0 * this->m_Z0 - m15 / m11);
-		}
-	}
-
-	/// <summary>
-	/// Recursive definition of determinate using expansion by minors.
-	/// </summary>
-	double Determinant(double a[4][4], int n)
+	// Find minor 1, 2.
+	for (int i = 0; i < 4; i++)
 	{
-			int i, j, j1, j2;
-			double d = 0;
-			double m[4][4] = 
-							{
-									{ ZERO, ZERO, ZERO, ZERO },
-									{ ZERO, ZERO, ZERO, ZERO },
-									{ ZERO, ZERO, ZERO, ZERO },
-									{ ZERO, ZERO, ZERO, ZERO }
-							};
-
-			if (n == 2)
-			{
-					// Terminate recursion.
-					d = a[0][0] * a[1][1] - a[1][0] * a[0][1];
-			}
-			else
-			{
-					d = 0;
-					for (j1 = 0; j1 < n; j1++) // Do each column.
-					{
-							for (i = 1; i < n; i++) // Create minor.
-							{
-									j2 = 0;
-									for (j = 0; j < n; j++)
-									{
-											if (j == j1) continue;
-											m[i - 1][j2] = a[i][j];
-											j2++;
-									}
-							}
-
-							// Sum (+/-)cofactor * minor.
-							d = d + pow(-1.0, j1) * a[0][j1] * this->Determinant(m, n - 1);
-					}
-			}
-
-			return d;
+		a[i][0] = P[i][0] * P[i][0] + P[i][1] * P[i][1] + P[i][2] * P[i][2];
+		a[i][1] = P[i][1];
+		a[i][2] = P[i][2];
+		a[i][3] = 1;
 	}
-};
+	m12 = Determinant(a, 4);
+
+	// Find minor 1, 3.
+	for (int i = 0; i < 4; i++)
+	{
+		a[i][0] = P[i][0] * P[i][0] + P[i][1] * P[i][1] + P[i][2] * P[i][2];
+		a[i][1] = P[i][0];
+		a[i][2] = P[i][2];
+		a[i][3] = 1;
+	}
+	m13 = Determinant(a, 4);
+
+	// Find minor 1, 4.
+	for (int i = 0; i < 4; i++)
+	{
+		a[i][0] = P[i][0] * P[i][0] + P[i][1] * P[i][1] + P[i][2] * P[i][2];
+		a[i][1] = P[i][0];
+		a[i][2] = P[i][1];
+		a[i][3] = 1;
+	}
+	m14 = Determinant(a, 4);
+
+	// Find minor 1, 5.
+	for (int i = 0; i < 4; i++)
+	{
+		a[i][0] = P[i][0] * P[i][0] + P[i][1] * P[i][1] + P[i][2] * P[i][2];
+		a[i][1] = P[i][0];
+		a[i][2] = P[i][1];
+		a[i][3] = P[i][2];
+	}
+	m15 = Determinant(a, 4);
+
+	// Calculate result.
+	if (m11 == 0) {
+		m_X0 = 0;
+		m_Y0 = 0;
+		m_Z0 = 0;
+		m_Radius = 0;
+	} else {
+		m_X0 = 0.5 * m12 / m11;
+		m_Y0 = -0.5 * m13 / m11;
+		m_Z0 = 0.5 * m14 / m11;
+		m_Radius = sqrt(m_X0 * m_X0 + m_Y0 * m_Y0 + m_Z0 * m_Z0 - m15 / m11);
+	}
+}
+
+/// <summary>
+/// Recursive definition of determinate using expansion by minors.
+/// </summary>
+double Determinant(double a[4][4], int n)
+{
+		int i, j, j1, j2;
+		double d = 0;
+		double m[4][4] = 
+						{
+								{ ZERO, ZERO, ZERO, ZERO },
+								{ ZERO, ZERO, ZERO, ZERO },
+								{ ZERO, ZERO, ZERO, ZERO },
+								{ ZERO, ZERO, ZERO, ZERO }
+						};
+
+		if (n == 2)
+		{
+				// Terminate recursion.
+				d = a[0][0] * a[1][1] - a[1][0] * a[0][1];
+		}
+		else
+		{
+				d = 0;
+				for (j1 = 0; j1 < n; j1++) // Do each column.
+				{
+						for (i = 1; i < n; i++) // Create minor.
+						{
+								j2 = 0;
+								for (j = 0; j < n; j++)
+								{
+										if (j == j1) continue;
+										m[i - 1][j2] = a[i][j];
+										j2++;
+								}
+						}
+
+						// Sum (+/-)cofactor * minor.
+						d = d + pow(-1.0, j1) * a[0][j1] * Determinant(m, n - 1);
+				}
+		}
+
+		return d;
+}
 
 int zero_x;
 int zero_y;
 int zero_z;
 float sensitivity;
 ADXL335 accelerometer;
-CircumcentreSolver solver;
 
 void setup() {
 	Serial.begin(9600);
@@ -200,10 +193,10 @@ void calibrate() {
 	pause();
 
 	int x, y, z;
-	double[] a = new double[3];
-	double[] b = new double[3];
-	double[] c = new double[3];
-	double[] d = new double[3];
+	int a[] = {0, 0, 0};
+	int b[] = {0, 0, 0};
+	int c[] = {0, 0, 0};
+	int d[] = {0, 0, 0};
 
 	Serial.println("Z up.");
 	pause();
@@ -221,13 +214,14 @@ void calibrate() {
 	pause();
 	accelerometer.getXYZ(&d[0], &d[1], &d[2]);
 
-	solver = new CircumcentreSolver(a, b, c, d);
+	CircumcentreSolver(a, b, c, d);
 
 	float zero_xv, zero_yv, zero_zv;
-	zero_xv = (float)solver.m_X0 * ADC_REF / ADC_AMPLITUDE;
-	zero_yv = (float)solver.m_Y0 * ADC_REF / ADC_AMPLITUDE;
-	zero_zv = (float)solver.m_Z0 * ADC_REF / ADC_AMPLITUDE;
-	sensitivity = (float)solver.m_Radius * ADC_REF / ADC_AMPLITUDE;
+	zero_xv = (float)m_X0 * ADC_REF / ADC_AMPLITUDE;
+	zero_yv = (float)m_Y0 * ADC_REF / ADC_AMPLITUDE;
+	zero_zv = (float)m_Z0 * ADC_REF / ADC_AMPLITUDE;
+	sensitivity = (float)m_Radius * ADC_REF / ADC_AMPLITUDE;
+	Serial.println();
 	Serial.println("please modify the macro definitions with the below in ADXL335.h");
 	Serial.print("  ZERO_X = ");
 	Serial.println(zero_xv);
